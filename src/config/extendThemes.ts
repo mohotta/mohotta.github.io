81fc6731bd5e65003d5e20535cfba6f6516a6e37
mohotta <kumudulaksitha@gmail.com>
2023-09-26 03:56:56 +0530

    init web

---
+++ src/config/extendThemes.ts
@@ -0,0 +1,12 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'JetBrains Mono', monospace`,
+    body: `'JetBrains Mono', monospace`,
+  },
+})
+
+export default theme
+
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import theme from './config/theme.ts'
+import extentTheme from './config/extendThemes.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={extentTheme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </React.StrictMode>,
