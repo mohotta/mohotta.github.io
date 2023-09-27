fdba0f3f73892f089faa3ed0cd35eff2278d0241
mohotta <kumudulaksitha@gmail.com>
2023-09-28 04:13:47 +0530

    added scroll

--- src/ScrlBtn.tsx
+++ src/ScrlBtn.tsx
@@ -5,21 +5,21 @@ import { gsap } from 'gsap';
 
 const ScrlBtn = () => {
 
-  const [isVisible, setIsVisible] = React.useState(false);
-  const scrollButton = useRef();
+    const [isVisible, setIsVisible] = React.useState(false);
+    const scrollButton = useRef<gsap.TweenTarget>(null);
 
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
 
-  useEffect(() => {
-    if (isVisible) {
-      gsap.to(scrollButton.current, {
-        duration: 0.5,
-        opacity: 1,
-        zIndex: 100,
-      });
+    useEffect(() => {
+        if (isVisible) {
+            gsap.to(scrollButton.current, {
+                duration: 0.5,
+                opacity: 1,
+                zIndex: 100,
+        });
     } else {
       gsap.to(scrollButton.current, {
         duration: 0.5,
@@ -27,40 +27,44 @@ const ScrlBtn = () => {
         zIndex: -1,
       });
     }
-  }, [isVisible]);
+    }, [isVisible]);
 
-  const handleScroll = () => {
-    if (window.scrollY > 100) {
-      setIsVisible(true);
-    } else {
-      setIsVisible(false);
+    const handleScroll = () => {
+        if (window.scrollY > 100) {
+        setIsVisible(true);
+        } else {
+        setIsVisible(false);
+        }
+    };
+
+    const handleClick = () => {
+        window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+        });
+    };
+
+    if (scrollButton.current !== undefined) {
+
+        return (
+            <IconButton
+            aria-label="scroll to top"
+            icon={<ArrowUpIcon />}
+            size="lg"
+            colorScheme="gray"
+            ref={scrollButton}
+            onClick={handleClick}
+            position="fixed"
+            bottom="4rem"
+            right="4rem"
+            zIndex="-1"
+            opacity="0"
+            />
+        );
+
     }
-  };
 
-  const handleClick = () => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: 'smooth',
-    });
-    scrollButton.current.blur();
-  };
-
-  return (
-    <IconButton
-      aria-label="scroll to top"
-      icon={<ArrowUpIcon />}
-      size="lg"
-      colorScheme="gray"
-      ref={scrollButton}
-      onClick={handleClick}
-      position="fixed"
-      bottom="4rem"
-      right="4rem"
-      zIndex="-1"
-      opacity="0"
-    />
-  );
 };
 
 export default ScrlBtn;
