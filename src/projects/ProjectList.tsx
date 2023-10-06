f3f5cdb39e8201c8517e87e01dbae1a16eaf3dac
mohotta <kumudulaksitha@gmail.com>
2023-10-06 23:02:27 +0530

    updated a project link

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -6,11 +6,18 @@ export default function ProjectList() {
 
     const projets = [
       {
-        title: 'EasyMail Client',
-        techStack: ['java', 'OOP'],
-        description: 'Email cliet for recieving emails using java. Used skills : java, object oriented programming, design patterns',
-        image: 'https://images.unsplash.com/photo-1603791440384-56cd371ee9a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZW1haWx8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
-        link: 'https://github.com/mohotta/easymail'
+        title: 'Speech Visualization (Ongoing)',
+        techStack: ['python', 'ML', 'dashboarding'],
+        description: 'dashboard showing the features of a audio file/live recording uploaded/recorded by the user',
+        image: 'https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZGFzaGJvYXJkfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+        link:'https://github.com/sp-vis'
+      },
+      {
+        title: 'Personal Portfolio',
+        techStack: ['typescript', 'REACT', 'ChakraUI'],
+        description: 'My personal portfolio website. Used skills: HTML/CSS, typescript, REACT, ChakraUI',
+        image: 'https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG9ydGZvbGlvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+        link: 'https://github.com/mohotta/mohotta.github.io'
       },
       {
         title: 'Airline Ticket Reservation System',
@@ -20,18 +27,11 @@ export default function ProjectList() {
         link: 'https://github.com/mohotta/DBMS-MiniProject'
       },
       {
-        title: 'Speech Visualization (Ongoing)',
-        techStack: ['python', 'ML', 'dashboarding'],
-        description: 'dashboard showing the features of a audio file/live recording uploaded/recorded by the user',
-        image: 'https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZGFzaGJvYXJkfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
-        link:'https://github.com/mushrafmim/speech-vis-be'
-      },
-      {
-        title: 'Personal Portfolio',
-        techStack: ['typescript', 'REACT', 'ChakraUI'],
-        description: 'My personal portfolio website. Used skills: HTML/CSS, typescript, REACT, ChakraUI',
-        image: 'https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG9ydGZvbGlvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
-        link: 'https://github.com/mohotta/mohotta.github.io'
+        title: 'EasyMail Client',
+        techStack: ['java', 'OOP'],
+        description: 'Email cliet for recieving emails using java. Used skills : java, object oriented programming, design patterns',
+        image: 'https://images.unsplash.com/photo-1603791440384-56cd371ee9a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZW1haWx8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
+        link: 'https://github.com/mohotta/easymail'
       },
       {
         title: 'Credit Card Fraud Detection',
