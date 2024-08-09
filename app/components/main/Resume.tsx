f01c42051d4b6c385ffdbacf26d8f7e9fbbf19c1
Kumudu Mohottala <kumudulaksitha@gmail.com>
2024-08-09 17:25:33 +0530

    typo fix

--- app/components/main/Resume.tsx
+++ app/components/main/Resume.tsx
@@ -158,7 +158,7 @@ const items = [
 const pros = [
   {
     id: 1,
-    name: 'Department of CSE - UOM',
+    name: 'Department of CSE - UoM',
     addr: 'Moratuwa, LK',
     pos: [
       {
@@ -187,7 +187,7 @@ const pros = [
   },
   {
     id: 3,
-    name: 'ACM Student Chapter - UOM',
+    name: 'ACM Student Chapter - UoM',
     addr: 'Moratuwa, LK',
     pos: [
       {
