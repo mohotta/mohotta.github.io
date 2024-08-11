c8dfaa93150264fe1bf9fb68fafd0ce8b383f15a
Kumudu Mohottala <kumudulaksitha@gmail.com>
2024-08-11 05:33:15 +0530

    new hero section - minor fix

--- app/components/main/HomePage.tsx
+++ app/components/main/HomePage.tsx
@@ -38,7 +38,7 @@ const HomePage = () => {
   return (
     <div id='home' className='min-h-screen w-full flex flex-col justify-center items-center section relative z-0'>
       <div className='flex flex-col justify-center items-start gap-4 max-w-[90%]'>
-        <Fade duration={1000}>
+        <Fade duration={1000} fraction={0.2}>
           <p className='text-xl font-bold tracking-widest text-step2-foreground/60'> Hi, I am </p>
           <h1 className='font-bold text-6xl text-step6-foreground dark:text-step3-foreground'> Kumudu Mohottala. </h1>
         </Fade>
@@ -60,21 +60,23 @@ const HomePage = () => {
             className={`text-3xl text-left font-medium w-full text-step2-foreground/75 h-full`}
           />
         </div>
-        <Fade duration={1000}>
+        <Fade duration={1000} fraction={0.2}>
           <p className='font-semibold text-justify max-w-[400px] text-step2-foreground/60 min-[604px]:max-w-[500px]'>
             I am a Computer Science & Engineering (Data Science) undergraduate of University of Moratuwa, Sri Lanka who searches for professional and learning opportunities to widen my horizons in software development & data science.
           </p>
         </Fade>
-        <div className='flex flex-row justify-center items-center w-full my-8 gap-2 sm:gap-4'>
-            <Button className='rounded-full flex gap-2' size={'lg'} variant={'secondary'} onClick={onClick}> learn more <RxDoubleArrowDown className='animate-bounce'/> </Button>
-            {
-              socials.map(item => (
-                <Button key={item.name} size="icon" variant={'secondary'} className='rounded-full'>
-                  <a href={item.link} target='_blank'>{item.icon}</a>
-                </Button>
-              ))
-            }
-        </div>
+        <Fade duration={1000} fraction={0.2} className='flex flex-row w-full justify-center items-center'>
+          <div className='flex flex-row justify-center items-center w-full my-8 gap-2 sm:gap-4'>
+              <Button className='rounded-full flex gap-2' size={'lg'} variant={'secondary'} onClick={onClick}> learn more <RxDoubleArrowDown className='animate-bounce'/> </Button>
+              {
+                socials.map(item => (
+                  <Button key={item.name} size="icon" variant={'secondary'} className='rounded-full'>
+                    <a href={item.link} target='_blank'>{item.icon}</a>
+                  </Button>
+                ))
+              }
+          </div>
+        </Fade>
       </div>
     </div>
   )
